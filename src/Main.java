d23e6a9ec0e2e02c2becf12c470ef38e0a7fd58a
danielo52 <gusolsdat@student.gu.se>
2020-01-20 15:45:32 +0100

    commit 4

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
 	System.out.println("Hello world hello world");
 	System.out.println("Hello");
 	System.out.println("This is branchTest");
+	System.out.println("Hello hello");
     }
 }
